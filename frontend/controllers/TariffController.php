9df3244cc6a1a784f13524405ccf19cff1fee52e
Anil <anil@techise.in>
2022-05-26 17:30:16 +0530

    Added Tariff Controller

---
+++ frontend/controllers/TariffController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace frontend\controllers;
+class TariffController extends Controller{
+
+    public function actionBasicdetails(){
+        return $this->render('', []);
+    }
+    public function actionContactdetails(){
+        return $this->render('', []);
+    }
+    public function actionGuestcount(){
+        return $this->render('', []);
+    }
+    public function actionAccommodation(){
+        return $this->render('', []);
+    }
+}
