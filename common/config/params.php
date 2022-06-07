96f32f057205be3459ea68b255c5dd96b80e3f4a
Anil <anil@techise.in>
2022-06-07 15:25:15 +0530

    Config changes

--- common/config/params.php
+++ common/config/params.php
@@ -5,5 +5,6 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
+    'user.emailValidationTokenExpire' => 172800,
     'user.passwordMinLength' => 8,
 ];
