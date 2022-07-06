ab208c4cf0d848dc465ea70482be52883396fe00
girisankar005 <girisankar005@gmail.com>
2022-07-06 23:56:05 +0530

    slab tariff dinner and meals  date-wise report styling

--- frontend/views/slab/dinner.php
+++ frontend/views/slab/dinner.php
@@ -13,78 +13,91 @@ $this->title = 'Operators';
 ?>
 
 <style>
-  .tariff_table { border-collapse: collapse; }
-  .tariff_table th, .tariff_table td { padding: 5px; border: solid 1px #777; }
-  .tariff_table th { background-color: lightblue; }
+    .tariff_table { border-collapse: collapse; margin: auto;
+        width: 100% !important; }
+    .tariff_table th, .tariff_table td { padding: 5px; border: solid 1px #777; }
+    .tariff_table th { background-color: #586ADA; }
+    table thead tr th:first-child {
+        border-top-left-radius: 0px;
+        border-bottom-left-radius: 0px;
+    }
+
+    table thead tr th:last-child {
+        border-top-right-radius: 0px;
+        border-bottom-right-radius: 0px;
+    }
 </style>
 
 <script>
-    $(document).ready(function() {    
+    $(document).ready(function() {
         $('input[name="daterange"]').datepicker({
             opens: 'left',
-            format: "d MM yyyy", 
+            format: "d MM yyyy",
             autoclose: true,
-        }, function(start, end, label) {        
+        }, function(start, end, label) {
     });
-  
+
     $('input[name="daterange"]').on('apply.daterangepicker', function(ev, picker) {
         $(this).val(picker.startDate.format('d MM yyyy'));
     });
-    
+
     $('input[name="daterange"]').on('cancel.daterangepicker', function(ev, picker) {
         $(this).val('');
-    });    
+    });
 } );
 
-function onChangeProperty(property){        
+function onChangeProperty(property){
        window.location.href = "/index.php?r=slab/dinner&id="+property.value;
 }
 </script>
 
-<div class="d_dark shadow p-2 ml-2">
-<?php $form = ActiveForm::begin(['id' => 'slab_meals','enableClientValidation' => true,'method' => 'post','action' => Url::to(['slab/dinner']).'&id='.$property_id]) ?>
-    <div class="row">
-        <div class="col-md-3 text-center mt-4">
-            <label>Property</lable>
+<div class="card">
+    <div class="card-body">
+        <?php $form = ActiveForm::begin(['id' => 'slab_meals','enableClientValidation' => true,'method' => 'post','action' => Url::to(['slab/dinner']).'&id='.$property_id]) ?>
+
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <label for="inputCity">Property</label>
+                <?= Html::dropDownList('property_id', $property_id, $properties_list, ['onchange' => 'onChangeProperty(this)','class'=>'form-control']) ?>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="inputZip">Month & Year</label>
+                <input type="text" class="form-control" name="daterange" value="<?= $date ?>" />
+
+            </div>
         </div>
-        <div class="col-md-3 text-center mt-4">
-            <?= Html::dropDownList('property_id', $property_id, $properties_list, ['onchange' => 'onChangeProperty(this)']) ?>
-        </div>
-        
-        <div class="col-md-3 text-center mt-4">
-            <label>Month & Year</lable>
-        </div>
-        <div class="col-md-3 text-center mt-4">
-            <input type="text" name="daterange" value="<?= $date ?>" />            
-        </div>
-        
-        <div class="col-md-3 text-center mt-4">
-            <button id="assign_slab" type="submit" class="btn btn-sm btn-save">Show</button>
-        </div>
-    </div>    
-<?php ActiveForm::end(); ?>
+        <button id="assign_slab" type="submit" class="btn btn-primary">Search</button>
+
+        <?php ActiveForm::end(); ?>
+
+
+    </div>
+</div>
+<div class="card">
+    <div class="card-body">
 
 <table class="tariff_table" id="tariff_list">
     <thead>
         <tr>
-            <th>Date</th>                                
-            <th>Particulars</th>            
+            <th>Date</th>
+            <th>Particulars</th>
             <th>Adult rate</th>
             <th>Child rate</th>
         </tr>
         </thead>
         <tbody>
-            <?php foreach ($dinner_tariff_array as $key => $slab) { 
+            <?php foreach ($dinner_tariff_array as $key => $slab) {
                 if($slab == NULL ) continue;
-                ?>                 
+                ?>
                 <tr>
                 <td><?= Carbon::parse($key)->format('d M Y')?> <br/> <?= Carbon::parse($key)->format('l') ?></td>
                 <td><?= $slab->name ?></td>
                 <td><?= $slab->rate_adult ?></td>
-                <td><?= $slab->rate_child ?></td>                
+                <td><?= $slab->rate_child ?></td>
                 </tr>
             <?php } ?>
         </tbody>
 </table>
 
+</div>
 </div>
--- frontend/views/slab/meals.php
+++ frontend/views/slab/meals.php
@@ -13,72 +13,87 @@ $this->title = 'Operators';
 ?>
 
 <style>
-  .tariff_table { border-collapse: collapse; }
+  .tariff_table { border-collapse: collapse; margin: auto;
+      width: 100% !important; }
   .tariff_table th, .tariff_table td { padding: 5px; border: solid 1px #777; }
-  .tariff_table th { background-color: lightblue; }
+  .tariff_table th { background-color: #586ADA; }
+  table thead tr th:first-child {
+      border-top-left-radius: 0px;
+      border-bottom-left-radius: 0px;
+  }
+
+  table thead tr th:last-child {
+      border-top-right-radius: 0px;
+      border-bottom-right-radius: 0px;
+  }
 </style>
 
 <script>
-    $(document).ready(function() {    
+    $(document).ready(function() {
         $('input[name="daterange"]').datepicker({
             opens: 'left',
-            format: "d MM yyyy", 
+            format: "d MM yyyy",
             autoclose: true,
-        }, function(start, end, label) {        
+        }, function(start, end, label) {
     });
-  
+
     $('input[name="daterange"]').on('apply.daterangepicker', function(ev, picker) {
         $(this).val(picker.startDate.format('d MM yyyy'));
     });
-    
+
     $('input[name="daterange"]').on('cancel.daterangepicker', function(ev, picker) {
         $(this).val('');
-    });    
+    });
 } );
 
-function onChangeProperty(property){        
+function onChangeProperty(property){
        window.location.href = "/index.php?r=slab/meals&id="+property.value;
 }
 </script>
 
-<div class="d_dark shadow p-2 ml-2">
-<?php $form = ActiveForm::begin(['id' => 'slab_meals','enableClientValidation' => true,'method' => 'post','action' => Url::to(['slab/meals']).'&id='.$property_id]) ?>
-    <div class="row">
-        <div class="col-md-3 text-center mt-4">
-            <label>Property</lable>
-        </div>
-        <div class="col-md-3 text-center mt-4">
-            <?= Html::dropDownList('property_id', $property_id, $properties_list, ['onchange' => 'onChangeProperty(this)']) ?>
-        </div>
-        
-        <div class="col-md-3 text-center mt-4">
-            <label>Month & Year</lable>
-        </div>
-        <div class="col-md-3 text-center mt-4">
-            <input type="text" name="daterange" value="<?= $date ?>" />            
-        </div>
-        
-        <div class="col-md-3 text-center mt-4">
-            <button id="assign_slab" type="submit" class="btn btn-sm btn-save">Show</button>
-        </div>
-    </div>    
-<?php ActiveForm::end(); ?>
+<div class="card">
+    <div class="card-body">
+        <?php $form = ActiveForm::begin(['id' => 'slab_meals','enableClientValidation' => true,'method' => 'post','action' => Url::to(['slab/meals']).'&id='.$property_id]) ?>
 
-<table class="tariff_table" id="tariff_list">
+
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <label for="inputCity">Property</label>
+                <?= Html::dropDownList('property_id', $property_id, $properties_list, ['onchange' => 'onChangeProperty(this)','class'=>'form-control']) ?>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="inputZip">Month & Year</label>
+                <input type="text" class="form-control" name="daterange" value="<?= $date ?>" />
+
+            </div>
+        </div>
+        <button id="assign_slab" type="submit" class="btn btn-primary">Search</button>
+
+        <?php ActiveForm::end(); ?>
+
+
+    </div>
+</div>
+
+
+    <div class="card">
+        <div class="card-body">
+
+  <table class="tariff_table" id="tariff_list">
     <thead>
         <tr>
-            <th>Date</th>                                
+            <th>Date</th>
             <th>BF Adult</th>
             <th>BF Child</th>
             <th>Lunch Adult</th>
-            <th>Lunch Child</th>                
-            <th>Dinner Adult</th> 
-            <th>Dinner Child</th>                
+            <th>Lunch Child</th>
+            <th>Dinner Adult</th>
+            <th>Dinner Child</th>
         </tr>
         </thead>
         <tbody>
-            <?php foreach ($meals_tariff_array as $key => $slabs) { ?> 
-                <?php                
+            <?php foreach ($meals_tariff_array as $key => $slabs) { ?>
+                <?php
                 $bf_adult = 0;
                 $bf_child = 0;
                 $lunch_adult = 0;
@@ -97,19 +112,20 @@ function onChangeProperty(property){
                         $dinner_adult = $slab->rate_adult;
                         $dinner_child = $slab->rate_child;
                     }
-                }                              
+                }
                 ?>
                 <tr>
                 <td><?= Carbon::parse($key)->format('d M Y')?> <br/> <?= Carbon::parse($key)->format('l') ?></td>
                 <td><?= $bf_adult ?></td>
                 <td><?= $bf_child ?></td>
-                <td><?= $lunch_adult ?></td>                
-                <td><?= $lunch_child ?></td> 
-                <td><?= $dinner_adult ?> </td>  
-                <td><?= $dinner_child ?> </td>  
+                <td><?= $lunch_adult ?></td>
+                <td><?= $lunch_child ?></td>
+                <td><?= $dinner_adult ?> </td>
+                <td><?= $dinner_child ?> </td>
                 </tr>
             <?php } ?>
         </tbody>
 </table>
 
-</div>
+  </div>
+  </div>
