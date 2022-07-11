7f55847db3dbd54fe5851c73c553dd247d12b8d7
Anil <anil@techise.in>
2022-07-11 21:40:08 +0530

    Hide previous button

--- frontend/views/tariff/add_hike_day_rate.php
+++ frontend/views/tariff/add_hike_day_rate.php
@@ -158,7 +158,7 @@ use frontend\models\tariff\roomTariffWeekdayhikeDays;
         } ?>
         <div class="row" style="margin-left: 4px;margin-bottom: 12px;">
             <div style="display: block;margin-right: 35px">
-                <BUTTON type="button" class="prevbutton" style="width: 80px;height: 30px" data-toggle="modal" data-target="#logoutModal"> Prev </BUTTON>
+                <!-- <BUTTON type="button" class="prevbutton" style="width: 80px;height: 30px" data-toggle="modal" data-target="#logoutModal"> Prev </BUTTON> -->
                 <?php if ($is_published != 1) { ?>
                 <BUTTON type="submit" class="buttonSave save-border" style="width: 80px;height: 30px" data-toggle="modal" data-target="#logoutModal"> Save </BUTTON>
                 <?php } ?>
