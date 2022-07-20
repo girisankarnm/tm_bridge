42e737c0c2c27cefdcfba1f3f3620e15d2058957
Lekshmi Priya Raveendran <lekshmipriyaraveendran@gmail.com>
2022-07-20 22:59:37 +0530

    button class and style changes

--- frontend/views/user/activation_failed.php
+++ frontend/views/user/activation_failed.php
@@ -1,6 +1,6 @@
 <?php
 use yii\bootstrap4\ActiveForm;
-
+$this->registerCssFile('/css/full-page.css');
 ?>
 
 <?php if (Yii::$app->session->hasFlash('resent_activation_success')): ?>
@@ -17,32 +17,29 @@ use yii\bootstrap4\ActiveForm;
     </div>
 <?php endif; ?>
 
-<div class="card" style="background-color: white; border-radius: 20px">
-<!--<div class="card col-12 mr-1 c_border">-->
-    <div class="card-header p-0 text-center">
-    <div class="login-logo">
-        <a href="<?=Yii::$app->homeUrl?>"><img  src="<?= Yii::$app->request->baseUrl . '/images/tm_bridge_logo.svg' ?>" alt="" width="200" height="100"></a>
-    </div>
-    <h5 class="text-secondary">Resend activation link?</h5>
-</div>
-    <div class="card-body mt-3">
 
+
+<div class="card" style=" background-color: white; border-radius: 20px">
     <?php $form = ActiveForm::begin(['id' => 'resend-verification-email-form']); ?>
-    <div class="row">
-        <h6 class="text-secondary type2 ml-4 mr-2">Your account validation failed. Would you like to receive activation link again?<br>
-            <center>Please fill out your email. </center></h6>
-    </div>
+    <div class="" style="padding: 28px;">
+        <div style="display: flex; justify-content: center; margin-bottom: 30px;">
+            <img src="/images/tm_bridge_logo.svg" class="logo-small">
+        </div>
 
-    <div class="form-group text-center">
-        <?= $form->field($model, 'email')->textInput(['autofocus' => true])->label(false) ?>
-    </div>
+        <div class="col-md-12" style="font-size: 14px; margin-bottom: 20px; text-align: center">
+            Your account validation failed. Would you like to receive activation link again?
+            Please fill out your email.
+        </div>
+        <div class="form-group text-center" style="margin-right: 30px; margin-left: 30px">
+            <?= $form->field($model, 'email')->textInput(['class' => 'login-input', 'placeholder'=>'Email', 'autofocus' => true])->label(false) ?>
+        </div>
 
-    <div class="form-group text-center">
-        <?= \yii\bootstrap4\Html::submitButton('Resend activation link', ['class' => 'btn btn-primary']) ?>
-    </div>
+        <div class="text-center" style="margin-bottom: 20px">
+            <?= \yii\bootstrap4\Html::submitButton('Resend activation link', ['class' => 'login-button', 'style'=>'width: 150px']) ?>
+        </div>
 
-<?php ActiveForm::end(); ?>
-</div>
+    </div>
+    <?php ActiveForm::end(); ?>
 </div>
 
 
