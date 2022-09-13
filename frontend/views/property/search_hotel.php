c162f8e87525b55df48ef6a9ba1de6e5ec2980f8
Aneesh <aneesh@cstechcs.com>
2022-09-13 13:56:40 +0530

    Tooltip Added

--- frontend/views/property/search_hotel.php
+++ frontend/views/property/search_hotel.php
@@ -719,7 +719,15 @@ frontend\assets\AppAsset::register($this);
                                             <div class="room-policy-occupancy-content-single">
                                                 <img src="images/search-popup/child-icon-1.svg" style="width:18px"
                                                     class="room-policy-occupancy-icon" alt="">
-                                                <span>8-15 YR</span>
+                                                <div class="tooltip-main">
+                                                    <span class="tooltip-button" aria-expanded="false">8-15 YR</span>
+                                                    <div class="tooltip-content top" role="tooltip" tabindex="0">
+                                                        <h2 class="tooltip-heading">Tooltip html item</h2>
+                                                        <p class="tooltip-para">Lorem ipsum dolor sit amet, consectetur
+                                                            adipiscing elit. Cras
+                                                            convallis sodales erat vel accumsan.</p>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -1511,6 +1519,9 @@ frontend\assets\AppAsset::register($this);
 </div>
 <!-- Flexbox container for aligning the toasts -->
 <script>
+$(window).on('load', function() {
+    $('#searchPopupModal').modal('show');
+});
 /* When the user clicks on the button,
     toggle between hiding and showing the dropdown content */
 function myFunctionSearch() {
@@ -1594,9 +1605,6 @@ function FunctionFilterView() {
     document.getElementById("myDropdown-view").classList.toggle("show-content");
 }
 
-$(window).on('load', function() {
-    $('#searchPopupModal').modal('show');
-});
 
 function viewDetails(id) {
     $(`#matching-boxes-main-${id}`).slideToggle(500);
@@ -1606,7 +1614,305 @@ function viewDetails(id) {
 function Toaster() {
     toastr.success("Hellow");
 }
-$(function() {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+// $(function() {
+//     $('[data-toggle="tooltip"]').tooltip()
+// })
+
+// const tooltips = document.querySelectorAll('.tooltip');
+
+// Array.prototype.forEach.call(tooltips, function(el, i) {
+//     let tooltipButton = el.querySelector('.tooltip-button'),
+//         tooltipContent = el.querySelector('.tooltip-content'),
+//         /* Search for last focussable element inside tooltip (so that we can remove the tooltip after next tab) */
+//         tooltipContentItemsFocusable = tooltipContent.querySelectorAll(
+//             'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'),
+//         tooltipContentItems = tooltipContentItemsFocusable[tooltipContentItemsFocusable.length - 1];
+
+//     /* set the tooltip position based on a top tooltip on screen */
+//     function setTooltipPosition() {
+//         /* if any positioning classes present -> remove them */
+//         positionClasses = ['top', 'right', 'bottom', 'left'];
+//         positionClasses.forEach(function(item) {
+//             tooltipContent.classList.remove(item);
+//         });
+
+
+//         /* Calculate tooltip space */
+
+//         const tooltipSpace = tooltipButton.getBoundingClientRect();
+//         const tooltipBox = tooltipContent.getBoundingClientRect();
+//         const tooltipRight = tooltipBox.right + tooltipBox.width;
+
+//         if (tooltipSpace.top > tooltipBox.height && tooltipBox.left > 0 && tooltipRight < window.innerWidth) {
+//             tooltipContent.classList.add('top')
+//         } else if (tooltipSpace.bottom > tooltipBox.height && tooltipBox.left > 0 && tooltipRight < window
+//             .innerWidth) {
+//             tooltipContent.classList.add('bottom')
+//         } else {
+//             if (tooltipBox.left > 0 && tooltipRight > window.innerWidth) {
+//                 tooltipContent.classList.add('left')
+//             } else if (tooltipBox.left < 0 && tooltipRight < window.innerWidth) {
+//                 tooltipContent.classList.add('right')
+//             } else {
+//                 tooltipContent.classList.add('bottom')
+//             }
+//         }
+//     }
+
+//     setTooltipPosition();
+//     /* retrigger position on resize  */
+//     window.addEventListener("resize", () => {
+//         setTooltipPosition();
+//     });
+
+//     let mouseOverTooltip = false,
+//         mouseOverTooltipButton = false,
+//         focusOnTooltip = false;
+//     tooltipButton.addEventListener('click', function(element) {
+//         clicktooltipContent()
+//     });
+//     tooltipButton.addEventListener('mouseover', function(element) {
+//         mouseOverTooltipButton = true;
+//         showtooltipContent()
+//     });
+//     tooltipButton.addEventListener('mouseout', function(element) {
+//         mouseOverTooltipButton = false;
+//         /* Set small timeout for removing the tooltip to make user able to interract  */
+//         setTimeout(function() {
+//             if (!mouseOverTooltip) {
+//                 hidetooltipContent()
+//             }
+//         }, 200);
+//     });
+//     tooltipButton.addEventListener('focus', function(element) {
+//         showtooltipContent()
+//     });
+//     tooltipButton.addEventListener('blur', function(element) {
+//         /* Set small timeout for removing the tooltip to make user able to interract  */
+//         setTimeout(function() {
+//             if (!focusOnTooltip) {
+//                 hidetooltipContent()
+//             }
+//         }, 200);
+//     });
+
+//     /* escape key closes tooltip  */
+//     tooltipButton.addEventListener('keyup', function(element) {
+//         if (event.keyCode == 27) {
+//             hidetooltipContent();
+//         };
+//     });
+//     tooltipContent.addEventListener('keyup', function(element) {
+//         if (event.keyCode == 27) {
+//             hidetooltipContent();
+//         };
+//     });
+
+
+//     /* default mouse enters and leave  */
+//     tooltipContent.addEventListener('mouseenter', function(element) {
+//         mouseOverTooltip = true;
+//     });
+//     tooltipContent.addEventListener('mouseleave', function(element) {
+//         mouseOverTooltip = false;
+//         /* Set small timeout for removing the tooltip to make user able to interract  */
+//         setTimeout(function() {
+//             if (!mouseOverTooltipButton) {
+//                 hidetooltipContent()
+//             }
+//         }, 200);
+
+//     });
+//     tooltipContent.addEventListener('focus', function(element) {
+//         focusOnTooltip = true;
+//         showtooltipContent()
+//     });
+//     if (tooltipContentItemsFocusable.length > 0) {
+//         tooltipContentItems.addEventListener('focus', function(element) {
+//             focusOnTooltip = true;
+//             showtooltipContent()
+//         });
+//         tooltipContentItems.addEventListener('blur', function(element) {
+//             focusOnTooltip = false;
+//             hidetooltipContent()
+//         });
+//     } else {
+//         tooltipContent.addEventListener('blur', function(element) {
+//             focusOnTooltip = false;
+//             hidetooltipContent()
+//         });
+//     }
+
+
+//     /* Functions for showing and hiding tooltip, add aria-expanded on button, not mandatory, but gives people with voice over an indicator something happened */
+//     function clicktooltipContent() {
+//         if (tooltipButton.getAttribute('aria-expanded') == 'true') {
+//             tooltipContent.classList.remove('active');
+//             tooltipButton.setAttribute('aria-expanded', 'false');
+//         } else {
+//             tooltipContent.classList.add('active');
+//             tooltipButton.setAttribute('aria-expanded', 'true');
+//         };
+//     }
+
+//     function showtooltipContent() {
+//         tooltipContent.classList.add('active');
+//         tooltipButton.setAttribute('aria-expanded', 'true');
+//     }
+
+//     function hidetooltipContent() {
+//         tooltipContent.classList.remove('active');
+//         tooltipButton.setAttribute('aria-expanded', 'false');
+//     }
+// });
+
+// BOF Tooltip JS
+const tooltips = document.querySelectorAll('.tooltip-main');
+
+Array.prototype.forEach.call(tooltips, function(el, i) {
+    let tooltipButton = el.querySelector('.tooltip-button'),
+        tooltipContent = el.querySelector('.tooltip-content'),
+        /* Search for last focussable element inside tooltip (so that we can remove the tooltip after next tab) */
+        tooltipContentItemsFocusable = tooltipContent.querySelectorAll(
+            'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'),
+        tooltipContentItems = tooltipContentItemsFocusable[tooltipContentItemsFocusable.length - 1];
+
+    /* set the tooltip position based on a top tooltip on screen */
+    function setTooltipPosition() {
+        /* if any positioning classes present -> remove them */
+        positionClasses = ['top', 'right', 'bottom', 'left'];
+        positionClasses.forEach(function(item) {
+            tooltipContent.classList.remove(item);
+        });
+
+
+        /* Calculate tooltip space */
+
+        const tooltipSpace = tooltipButton.getBoundingClientRect();
+        const tooltipBox = tooltipContent.getBoundingClientRect();
+        const tooltipRight = tooltipBox.right + tooltipBox.width;
+
+        if (tooltipSpace.top > tooltipBox.height && tooltipBox.left > 0 && tooltipRight < window.innerWidth) {
+            tooltipContent.classList.add('top')
+        } else if (tooltipSpace.bottom > tooltipBox.height && tooltipBox.left > 0 && tooltipRight < window
+            .innerWidth) {
+            tooltipContent.classList.add('bottom')
+        } else {
+            if (tooltipBox.left > 0 && tooltipRight > window.innerWidth) {
+                tooltipContent.classList.add('left')
+            } else if (tooltipBox.left < 0 && tooltipRight < window.innerWidth) {
+                tooltipContent.classList.add('right')
+            } else {
+                tooltipContent.classList.add('bottom')
+            }
+        }
+    }
+
+    setTooltipPosition();
+    /* retrigger position on resize  */
+    window.addEventListener("resize", () => {
+        setTooltipPosition();
+    });
+
+    let mouseOverTooltip = false,
+        mouseOverTooltipButton = false,
+        focusOnTooltip = false;
+    tooltipButton.addEventListener('click', function(element) {
+        clicktooltipContent()
+    });
+    tooltipButton.addEventListener('mouseover', function(element) {
+        mouseOverTooltipButton = true;
+        showtooltipContent()
+    });
+    tooltipButton.addEventListener('mouseout', function(element) {
+        mouseOverTooltipButton = false;
+        /* Set small timeout for removing the tooltip to make user able to interract  */
+        setTimeout(function() {
+            if (!mouseOverTooltip) {
+                hidetooltipContent()
+            }
+        }, 200);
+    });
+    tooltipButton.addEventListener('focus', function(element) {
+        showtooltipContent()
+    });
+    tooltipButton.addEventListener('blur', function(element) {
+        /* Set small timeout for removing the tooltip to make user able to interract  */
+        setTimeout(function() {
+            if (!focusOnTooltip) {
+                hidetooltipContent()
+            }
+        }, 200);
+    });
+
+    /* escape key closes tooltip  */
+    tooltipButton.addEventListener('keyup', function(element) {
+        if (event.keyCode == 27) {
+            hidetooltipContent();
+        };
+    });
+    tooltipContent.addEventListener('keyup', function(element) {
+        if (event.keyCode == 27) {
+            hidetooltipContent();
+        };
+    });
+
+
+    /* default mouse enters and leave  */
+    tooltipContent.addEventListener('mouseenter', function(element) {
+        mouseOverTooltip = true;
+    });
+    tooltipContent.addEventListener('mouseleave', function(element) {
+        mouseOverTooltip = false;
+        /* Set small timeout for removing the tooltip to make user able to interract  */
+        setTimeout(function() {
+            if (!mouseOverTooltipButton) {
+                hidetooltipContent()
+            }
+        }, 200);
+
+    });
+    tooltipContent.addEventListener('focus', function(element) {
+        focusOnTooltip = true;
+        showtooltipContent()
+    });
+    if (tooltipContentItemsFocusable.length > 0) {
+        tooltipContentItems.addEventListener('focus', function(element) {
+            focusOnTooltip = true;
+            showtooltipContent()
+        });
+        tooltipContentItems.addEventListener('blur', function(element) {
+            focusOnTooltip = false;
+            hidetooltipContent()
+        });
+    } else {
+        tooltipContent.addEventListener('blur', function(element) {
+            focusOnTooltip = false;
+            hidetooltipContent()
+        });
+    }
+
+
+    /* Functions for showing and hiding tooltip, add aria-expanded on button, not mandatory, but gives people with voice over an indicator something happened */
+    function clicktooltipContent() {
+        if (tooltipButton.getAttribute('aria-expanded') == 'true') {
+            tooltipContent.classList.remove('active');
+            tooltipButton.setAttribute('aria-expanded', 'false');
+        } else {
+            tooltipContent.classList.add('active');
+            tooltipButton.setAttribute('aria-expanded', 'true');
+        };
+    }
+
+    function showtooltipContent() {
+        tooltipContent.classList.add('active');
+        tooltipButton.setAttribute('aria-expanded', 'true');
+    }
+
+    function hidetooltipContent() {
+        tooltipContent.classList.remove('active');
+        tooltipButton.setAttribute('aria-expanded', 'false');
+    }
+});
+// EOF Tooltip JS
 </script>
