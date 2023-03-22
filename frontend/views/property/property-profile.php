5233dff61e46aafff2e7704a4f7173b69075b114
Aneesh <aneesh@cstechcs.com>
2023-03-23 02:26:55 +0530

    Property Profile page

--- frontend/views/property/property-profile.php
+++ frontend/views/property/property-profile.php
@@ -1,5 +1,4 @@
 <?php
-frontend\assets\AppAsset::register($this);
 $this->registerCssFile('/css/ppe/style.css');
 $this->registerCssFile('/css/property-profile/property-profile.css');
 ?>
@@ -18,7 +17,7 @@ $this->registerCssFile('/css/property-profile/property-profile.css');
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-9">
             <div class="property-details-wrapper">
                 <div class="property-details-header">
                     <div class="hotel-category-wrapper">
@@ -64,8 +63,142 @@ $this->registerCssFile('/css/property-profile/property-profile.css');
                         </ul>
                     </div>
                 </div>
+                <div class="facilities-wrapper">
+                    <div class="room-content-details">
+                        <div class="room-content-single">
+                            <figure><img src="images/property-profile/icons/meal-icon.svg" alt=""></figure>
+                            <article>
+                                <h6>Meal Option</h6>
+                                <h3>MAP (Breakfast + Dinner)</h3>
+                            </article>
+                        </div>
+                        <div class="room-content-single">
+                            <figure><img src="images/property-profile/icons/document-text.svg" alt=""></figure>
+                            <article>
+                                <h6>Nationality Based Tariff</h6>
+                                <h3>Same tariff for all nationalities</h3>
+                            </article>
+                        </div>
+                    </div>
+                    <h4 class="facilities-heading">Cancellation policy</h4>
+                    <ul class="policy-list">
+                        <li><img src="images/property-profile/icons/tick-square-icon.svg" alt="">
+                            <span class="color-blue">100% refund if 60 days</span> prior to arrival date
+                        </li>
+                        <li><img src="images/property-profile/icons/tick-square-icon.svg" alt="">
+                            <span class="color-blue">No refund if 10 days</span> prior to arrival date
+                        </li>
+                        <li><img src="images/property-profile/icons/tick-square-icon.svg" alt="">
+                            <span class="color-blue">30% if refund if 59 days</span> to 11 days prior to arrival date
+                        </li>
+                    </ul>
+                </div>
+                <div class="facilities-wrapper">
+                    <div class="property-about">
+                        <h4 class="facilities-heading">About</h4>
+                        <p>Located in Munnar, 32 km from Munnar Tea Museum, Pleasant Hill Resort provides accommodation
+                            with free bikes, free private parking, an outdoor swimming pool and a fitness centre. Each
+                            accommodation at the 4-star resort has garden views, and guests can enjoy access to a garden
+                            and to a terrace. The accommodation offers a 24-hour front desk, airport transfers, room
+                            service and free WiFi throughout the property.</p>
+                        <p>At the resort, each room is fitted with a desk, a flat-screen TV, a private bathroom, bed
+                            linen, towels and a balcony with a mountain view. Guest rooms in Pleasant Hill Resort are
+                            equipped with free toiletries and a computer.
+                            The daily breakfast offers buffet, continental or American options. At the accommodation you
+                            will find a restaurant serving Indian, local and international cuisine. Vegetarian, kosher
+                            and halal options can also be requested.
+                            The area is popular for cycling, and car hire is available at this 4-star resort.</p>
+                        <p>Guests can make sightseeing and ticketing arrangements at the tour desk, or conduct business
+                            at the business centre.
+                            Mattupetty Dam is 40 km from Pleasant Hill Resort, while Anamudi Peak is 46 km away. The
+                            nearest airport is Cochin International Airport, 104 km from the resort.</p>
+                        <p>Couples particularly like the location — they rated it 9.3 for a two-person trip.</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-lg-4"></div>
+        <div class="col-lg-3">
+            <div class="rating-card">
+                <figure>4.3 <img src="images/property-profile/icons/star.svg" alt=""></figure>
+                <article>
+                    <h3>Very good</h3>
+                    <h5>5879 Rating</h5>
+                </article>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="rooms-list-area">
+                <div class="rooms-list-single">
+                    <div class="rooms-list-content-single room-name-details">
+                        <figure>
+                            <img src="images/property-profile/room-img-01.jpg" alt="">
+                            <div class="show-all-photos"><img src="images/property-profile/icons/grid.svg" alt=""> Show
+                                all Photos</div>
+                        </figure>
+                        <article>
+                            <h4>Deluxe Double Room with Balcony</h4>
+                            <h5>1 Large double bed</h5>
+                        </article>
+                    </div>
+                    <div class="rooms-list-content-single aminities-area">
+                        <div class="room-details-heading">Room Amenities</div>
+                        <ul class="aminities-list">
+                            <li>Free Wi-Fi</li>
+                            <li>Mineral Water - additional charge</li>
+                            <li>Air Conditioning</li>
+                            <li>Bathroom</li>
+                            <li>Air Conditioning</li>
+                            <li>Charging Points</li>
+                            <li>Living Area</li>
+                        </ul>
+                    </div>
+                    <div class="rooms-list-content-single highlights-area">
+                        <div class="room-details-heading">Room Highlights</div>
+                        <ul class="highlights-list">
+                            <li><img src="images/property-profile/icons/tick-icon.svg" alt=""> Room type:
+                                <div class="strong">Floating cottage</div>
+                            </li>
+                            <li><img src="images/property-profile/icons/tick-icon.svg" alt=""> Room View:
+                                <div class="strong">Beach</div>
+                            </li>
+                            <li><img src="images/property-profile/icons/tick-icon.svg" alt=""> Inventory:
+                                <div class="strong">12</div>
+                            </li>
+                            <li><img src="images/property-profile/icons/tick-icon.svg" alt=""> Average room size:
+                                <div class="strong">1250 Sq. Ft</div>
+                            </li>
+                            <li><img src="images/property-profile/icons/tick-icon.svg" alt=""> Default Meal Plan:
+                                <div class="strong">CP (Breakfast)</div>
+                            </li>
+                            <li><img src="images/property-profile/icons/tick-icon.svg" alt=""> Entry Restriction:
+                                <div class="strong">Admission is restricted f
+                                    or guests under 7 years</div>
+                            </li>
+                            <li><img src="images/property-profile/icons/tick-icon.svg" alt=""> <strong>Free cancellation
+                                    until 23:59 on 8 March 2023</strong></li>
+                        </ul>
+                    </div>
+                    <div class="rooms-list-content-single room-details-area">
+                        <div class="room-details-single">
+                            <div class="room-details-heading">Defacult Occupancy</div>
+                            <ul class="room-details-list">
+                                <li><img src="images/property-profile/icons/bed.svg" alt=""> BD: 2</li>
+                                <li>EB: 1</li>
+                                <li>SB: 1</li>
+                            </ul>
+                        </div>
+                        <div class="room-details-single">
+                            <div class="room-details-heading">Room’s Child Policy</div>
+                            <ul class="room-details-list">
+                                <li><img src="images/property-profile/icons/child.svg" alt=""> 0-5</li>
+                                <li><img src="images/property-profile/icons/adult.svg" alt=""> 6-11</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
