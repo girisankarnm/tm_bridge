571a772cd503ad158be4d81c17e351be8567f5d4
Aneesh <aneesh@cstechcs.com>
2023-04-20 13:29:09 +0530

    Modal changes

--- frontend/views/property/property-profile.php
+++ frontend/views/property/property-profile.php
@@ -814,6 +814,12 @@ $this->registerCssFile('/css/property-profile/property-profile.css');
         </div>
     </div>
 </div>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary video-btn" data-toggle="modal"
+    data-src="https://www.youtube.com/embed/NFWSFbqL0A0" data-target="#videoModal">
+    Play Video 1 - autoplay
+</button>
 <!-- BOF Modal -->
 <div class="modal bg-white-modal fade" id="allPhotosModal" tabindex="-1" role="dialog"
     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -879,3 +885,48 @@ $this->registerCssFile('/css/property-profile/property-profile.css');
     </div>
 </div>
 <!-- EOF Modal -->
+<!-- BOF Video Modal -->
+<div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                    style="font-size: 18pmaipx;">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <!-- 16:9 aspect ratio -->
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always"
+                        allow="autoplay"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- EOF Video Modal -->
+
+<script>
+$(window).on('load', function() {
+    $('#videoModal').modal('show');
+});
+$(document).ready(function() {
+    // Gets the video src from the data-src on each button
+    var $videoSrc;
+    $('.video-btn').click(function() {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    // when the modal is opened autoplay it  
+    $('#myModal').on('shown.bs.modal', function(e) {
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide.bs.modal', function(e) {
+        // a poor man's stop video
+        $("#video").attr('src', $videoSrc);
+    })
+    // document ready  
+});
+</script>
